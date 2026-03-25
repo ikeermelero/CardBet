749e6b9e4a4a42abd152d08a571ea1d10b3ad9fb
ikeermelero <ikermelero57@gmail.com>
2026-03-25 10:10:27 +0100

    nueva estructura

--- db/seed.sql
+++
@@ -1,6 +0,0 @@
-USE card_platform;
-
-INSERT INTO games (slug, name, description) VALUES
-  ('blackjack', 'Blackjack', 'Plántate o pide. Llega a 21 sin pasarte.'),
-  ('poker',     'Poker',     'Texas Hold\'em. Próximamente.'),
-  ('baccarat',  'Baccarat',  'Punto o Banca. Próximamente.');
